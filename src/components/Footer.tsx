802ee1b1233a8dacf694067d49bd873dd8eb6f04
Voosaki <kamil.szczukowski@interia.pl>
2024-01-15 15:37:58 +0100

    Split Home.tsx to smaller components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import githubSVG from "../assets/images/github.svg";
+import coffeeSVG from "../assets/images/coffee.svg";
+import infoSVG from "../assets/images/info.svg";
+import ExternalLink from "../components/ExternalLink";
+import ColoredImage from "../components/ColoredImage";
+
+const Footer = () => (
+  <footer>
+    <div className="useful-links">
+      <ColoredImage
+        href="https://github.com/Woosaki"
+        src={githubSVG}
+        alt="Github Image"
+        backgroundColor="#000"
+      />
+      <ColoredImage
+        href="https://ko-fi.com/wowdlegame"
+        src={coffeeSVG}
+        alt="Coffee Image"
+        backgroundColor="#ace4aa"
+      />
+      <ColoredImage
+        href="https://www.wp.pl/"
+        src={infoSVG}
+        alt="Info Image"
+        backgroundColor="#fcc41c"
+      />
+    </div>
+    <p>Kamil Szczukowski, 186714</p>
+    <p className="inspired">
+      Inspired by <ExternalLink href="https://loldle.net/">Loldle</ExternalLink>{" "}
+      and{" "}
+      <ExternalLink href="https://www.nytimes.com/games/wordle/index.html">
+        Wordle
+      </ExternalLink>
+    </p>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/warcraftdleLogo.png";
+
+const Header = () => (
+  <header>
+    <Link to="/">
+      <img src={logo} alt="Warcraftdle Logo" className="logo" />
+    </Link>
+  </header>
+);
+
+export default Header;
