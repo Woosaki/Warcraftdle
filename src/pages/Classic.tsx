c19871999537b1581d790e75f1d2d8b90dec5e65
Voosaki <kamil.szczukowski@interia.pl>
2024-01-19 11:57:18 +0100

    Create an input with character's filtering

--- src/pages/Classic.tsx
+++ src/pages/Classic.tsx
@@ -1,3 +1,4 @@
+import "../assets/styles/Classic.css";
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -33,18 +34,40 @@ const Classic = () => {
   return (
     <>
       <Header />
-      <main>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Type character name ..."
-        />
-        {characters.map((character) => (
-          <div key={character.id}>
-            <h2>{character.name}</h2>
+      <main className="classic-game">
+        <p className="text-shadow">Guess World of Warcraft character!</p>
+        <div className="input-container">
+          <input
+            className="input-field"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type character name ..."
+          />
+          <button
+            onClick={() => console.log(characters[0])}
+            className="submit-button"
+          >
+            Submit
+          </button>
+        </div>
+        {input && (
+          <div className="characters-list">
+            <div style={{ height: "8px" }}></div>
+            {characters.length === 0 && (
+              <div className="no-character-found">No character found.</div>
+            )}
+            {characters.length > 0 &&
+              characters.map((character) => {
+                return (
+                  <div key={character.id} className="character">
+                    {character.name}
+                  </div>
+                );
+              })}
+            <div style={{ height: "8px" }}></div>
           </div>
-        ))}
+        )}
       </main>
       <Footer />
     </>
